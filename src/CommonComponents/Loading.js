4181fe52d7842b531236fd2b8cb73aa3059433e8
Bhoomi Kathiriya <v.bhoomi@virtualgodown.com>
2019-02-17 13:21:19 +0530

    boilerplate

---
+++ src/CommonComponents/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import img from '../loading_spinner.gif'
+
+let Laoading = ({ loading }) => (
+
+  loading ?
+    <div style={{ textAlign: 'center' }}>
+      <img src={img} alt='loading' />
+      <h1>LOADING</h1>
+    </div> :
+    null
+);
+
+export default Laoading;
